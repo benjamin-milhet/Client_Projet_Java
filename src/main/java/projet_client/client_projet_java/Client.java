01dd559e9f15a9534ddeaddc30b8bca88c6577b9
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-08 14:54:53 +0200

    client connect to the server

--- src/main/java/projet_client/client_projet_java/Client.java
+++ src/main/java/projet_client/client_projet_java/Client.java
@@ -6,6 +6,6 @@ import java.io.*;
 public class Client {
 
     public Client() throws IOException {
-        Socket socket = new Socket("localhost", 8080);
+        Socket socket = new Socket("localhost", 8080); // Permet de se conncter à un serveur distant sur le port 8080
     }
 }
