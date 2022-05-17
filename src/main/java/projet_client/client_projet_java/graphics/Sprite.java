6e762b31f3620b10696841b4d64bf4ded5e86cd9
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-17 09:50:09 +0200

    clean warning Sprite.java

--- src/main/java/projet_client/client_projet_java/graphics/Sprite.java
+++ src/main/java/projet_client/client_projet_java/graphics/Sprite.java
@@ -2,7 +2,7 @@ package projet_client.client_projet_java.graphics;
 
 import javafx.scene.image.Image;
 
-import java.io.IOException;
+import java.util.Objects;
 
 import static projet_client.client_projet_java.Game.SCALE;
 
@@ -14,13 +14,10 @@ public class Sprite {
 
     private final Image image;
 
-    public Sprite(String path, int width, int height) throws IOException {
-            this.image = new Image(getClass().getResource(path).toExternalForm(), width * SCALE, height * SCALE, false, false);
+    public Sprite(String path, int width, int height) {
+        this.image = new Image(Objects.requireNonNull(getClass().getResource(path)).toExternalForm(), width * SCALE, height * SCALE, false, false);
 
-            if (this.image == null) {
-                throw new IOException("Impossible de charge l'image");
-            }
-            this.path = path;
+        this.path = path;
             this.width = this.image.getWidth();
             this.height = this.image.getHeight();
 
