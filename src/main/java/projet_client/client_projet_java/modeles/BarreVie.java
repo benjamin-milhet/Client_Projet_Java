a38f855b7f49d90a0b2b507573c1b9ccab7c086e
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-17 09:38:35 +0200

    clean warning BarreVie.java

--- src/main/java/projet_client/client_projet_java/modeles/BarreVie.java
+++ src/main/java/projet_client/client_projet_java/modeles/BarreVie.java
@@ -3,15 +3,12 @@ package projet_client.client_projet_java.modeles;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-import java.awt.desktop.SystemSleepEvent;
-
 import static projet_client.client_projet_java.Game.SCALE;
 
 public class BarreVie extends Objet{
 
-    private int vie;
-    private int vieMax;
-    private String position;
+    private final int vie, vieMax;
+    private final String position;
 
     public BarreVie(int vie, int vieMax, int x, int y, String position) {
         this.vie = vie-20;
@@ -32,17 +29,17 @@ public class BarreVie extends Objet{
         Color rouge = Color.rgb(93, 9, 35); // Color white
         graphics.setFill(rouge);
         if(this.position.equals("droite")){
-            graphics.fillRoundRect(this.x - (this.vieMax * SCALE + (1 * SCALE)), this.y, this.vieMax * SCALE + (1 * SCALE), 15 * SCALE, 20, 20);
+            graphics.fillRoundRect(this.x - (this.vieMax * SCALE + (SCALE)), this.y, this.vieMax * SCALE + (SCALE), 15 * SCALE, 20, 20);
         } else if(this.position.equals("gauche")){
-            graphics.fillRoundRect(this.x + 100, this.y, this.vieMax * SCALE + (1 * SCALE), 15 * SCALE, 20, 20);
+            graphics.fillRoundRect(this.x + 100, this.y, this.vieMax * SCALE + (SCALE), 15 * SCALE, 20, 20);
         }
 
         Color vert = Color.rgb(55, 90, 59); // Color white
         graphics.setFill(vert);
         if(this.position.equals("droite")){
-            graphics.fillRoundRect(this.x + (SCALE * (this.vieMax - this.vie)) - (this.vieMax * SCALE) - (1 * SCALE), this.y + (1.5 * SCALE), this.vie * SCALE, 12 * SCALE, 20, 20);
+            graphics.fillRoundRect(this.x + (SCALE * (this.vieMax - this.vie)) - (this.vieMax * SCALE) - (SCALE), this.y + (1.5 * SCALE), this.vie * SCALE, 12 * SCALE, 20, 20);
         } else if(this.position.equals("gauche")){
-            graphics.fillRoundRect(this.x + 100 + (1 * SCALE), this.y + (1.5 * SCALE), this.vie * SCALE, 12 * SCALE, 20, 20);
+            graphics.fillRoundRect(this.x + 100 + (SCALE), this.y + (1.5 * SCALE), this.vie * SCALE, 12 * SCALE, 20, 20);
         }
     }
 }
