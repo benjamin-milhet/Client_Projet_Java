23892fc224767c4d957813f95a481074a8986c34
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-17 09:36:35 +0200

    clean warning FabriqueEntity.java

--- src/main/java/projet_client/client_projet_java/modeles/entity/FabriqueEntity.java
+++ src/main/java/projet_client/client_projet_java/modeles/entity/FabriqueEntity.java
@@ -1,8 +1,6 @@
 package projet_client.client_projet_java.modeles.entity;
 
 import projet_client.client_projet_java.input.Keyboard;
-import projet_client.client_projet_java.modeles.entity.Archer;
-import projet_client.client_projet_java.modeles.entity.Entity;
 
 import static projet_client.client_projet_java.Game.SCALE;
 
@@ -11,11 +9,10 @@ public class FabriqueEntity {
     public static Entity fabrique(String nom, int speed, int x, int y, int life, Keyboard keyboard) {
         Entity res = null;
 
-        switch (nom) {
-            case "Archer":
-                return new Archer("archer", speed, x * SCALE,y * SCALE,life, keyboard);
-        }
+        return switch (nom) {
+            case "Archer" -> new Archer("archer", speed, x * SCALE, y * SCALE, life, keyboard);
+            default -> res;
+        };
 
-        return res;
     }
 }
